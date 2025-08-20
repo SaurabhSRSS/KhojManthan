8af450017ed85f7ef1895bf5853f62fc7ed38f68
SRSS <srss.casm@gmail.com>
2025-08-20 15:54:55 +0530

    Update kmssrs.js

--- app/kmssrs.js
+++ app/kmssrs.js
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <main style={{ textAlign: "center", padding: "50px" }}>
-      <h1>🚀 Welcome to KhojManthan</h1>
-      <p>India’s First Indigenous File Search Engine by SRSS</p>
-    </main>
-  );
-}
+
