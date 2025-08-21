76c25a773490b226f5c6f6b753c225479fe145a6
SRSS <srss.casm@gmail.com>
2025-08-21 10:31:49 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
 // app/layout.tsx
 import "./globals.css";
 import type { Metadata } from "next";
+import type { ReactNode } from "react";
 
 export const metadata: Metadata = {
   title: "KhojManthan",
   description: "India’s First Indigenous File Search Engine",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
