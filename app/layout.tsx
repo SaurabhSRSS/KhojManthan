09ed82735c2ac6d35d24119f83bb27aa0a374d47
SRSS <srss.casm@gmail.com>
2025-08-21 11:04:29 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,17 @@
-// app/layout.tsx
 import "./globals.css";
-import type { Metadata } from "next";
-import type { ReactNode } from "react";
+import { ReactNode } from "react";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "KhojManthan",
-  description: "India’s First Indigenous File Search Engine",
-  icons: {
-    icon: "/favicon.png",
-  },
+  description: "SRSS Search Engine Project",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body>{children}</body>
     </html>
   );
-}
+      }
