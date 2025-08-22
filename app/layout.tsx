0a549d540b5474581c65c013a58f71cbafda5b7b
SRSS <srss.casm@gmail.com>
2025-08-22 17:11:31 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export const metadata: Metadata = {
   title: "KhojManthan",
